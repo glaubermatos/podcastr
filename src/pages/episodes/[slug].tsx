cd16c464e8bc458dec59902b6b2b3771d52d1776
glaubermatos <glaub.oliveira@hotmail.com>
2021-04-27 17:32:58 -0300

    Create episode page

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,10 +1,14 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
 import api from '../../services/api'
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString'
+
+import styles from './Episode.module.scss'
 
 type Episode = {
     id: string,
@@ -25,7 +29,39 @@ type EpisodeProps = {
 export default function Episode({ episode }: EpisodeProps) {
 
     return (
-        <span>{JSON.stringify(episode)}</span>
+        <div className={styles.episodeContainer}>
+            <div className={styles.episode}>
+                <div className={styles.thumbnailContainer}>
+                    <Link href="/">
+                        <button type="button">
+                            <img src="/arrow-left.svg" alt="Voltar" />
+                        </button>
+                    </Link>
+                    <Image
+                        width={700}
+                        height={160}
+                        src={episode.thumbnail}
+                        objectFit="cover"
+                    />
+                    <button type="button">
+                        <img src="/play.svg" alt="Tocar" />
+                    </button>
+                </div>
+
+                <header>
+                    <h1>{episode.title}</h1>
+                    <span>{episode.members}</span>
+                    <span>{episode.publishedAt}</span>
+                    <span>{episode.durationAsString}</span>
+                </header>
+
+                <div
+                    className={styles.description}
+                    dangerouslySetInnerHTML={{ __html: episode.description }}
+                />
+            </div>
+        </div>
+
     )
 }
 
